035e09f9f97f980e31238c430d9d680db52c8f4d
Carol Masvisvi <carolmasvisvi@gmail.com>
2026-04-22 17:17:29 +0200

    deployment config

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from services.weather_service import get_weather, get_forecast
 from utils.scene_mapper import map_condition_to_theme
+import os
 
 app = Flask(__name__)
 
@@ -39,4 +40,5 @@ def index():
         )
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
